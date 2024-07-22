8d01ca44c239be62c71f8038a946756226a38522
Jason Vellucci <106223269+returnMarcco@users.noreply.github.com>
2024-07-22 22:39:32 +1000

    Delete modules-demo directory

--- modules-demo/my-module.js
+++
@@ -1,9 +0,0 @@
-// learn Modules
-
-// Exports
-
-// Old way
-exports.myExportedGoodbyeWorldFunction = () => 'Goodbye, world!';
-
-// ES6 
-export const myExportedHelloWorldfunction = () => "Hello, world!";
